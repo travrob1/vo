ae97d4640fde9434ce392538a20ca7380b8bb69e
Travis Robison <travis.robison@progfinance.com>
2016-10-07 15:08:35 -0600

    adding auth changes

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,9 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '1003004099767925', // your App ID
 		'clientSecret' 	: '7179c53cd3b4e8df65b1f01d07f6b3ad', // your App Secret
-		'callbackURL' 	: 'http://localhost:4000/auth/facebook/callback'
+		'callbackURL' 	: 'http://localhost:4000/auth/facebook/callback',
+    	'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,gender,last_name,link,locale,name,timezone,verified,email,updated_time,picture.type(large)'
+
 	},
 
 	'twitterAuth' : {
