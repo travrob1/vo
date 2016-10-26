6a166489071637b7b2e6b6fa4cc2233d36c3be92
Chris David <chris.david@progfinance.com>
2016-10-25 21:09:12 -0700

    initial swaggerization

---
+++ api/data/profile.js
@@ -0,0 +1,96 @@
+'use strict';
+var Mockgen = require('./mockgen.js');
+/**
+ * Operations on /profile
+ */
+module.exports = {
+    /**
+     * summary: 
+     * description: Gets `Profile` objects.
+Optional query param of **size** determines
+size of returned array
+
+     * parameters: size
+     * produces: 
+     * responses: 200, default
+     * operationId: 
+     */
+    get: {
+        200: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/profile',
+                operation: 'get',
+                response: '200'
+            }, callback);
+        },
+        default: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/profile',
+                operation: 'get',
+                response: 'default'
+            }, callback);
+        }
+    },
+    /**
+     * summary: 
+     * description: Create a new `profile`
+
+     * parameters: profile
+     * produces: 
+     * responses: 200
+     * operationId: 
+     */
+    post: {
+        200: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/profile',
+                operation: 'post',
+                response: '200'
+            }, callback);
+        }
+    },
+    /**
+     * summary: 
+     * description: 
+     * parameters: profile
+     * produces: 
+     * responses: 200, default
+     * operationId: 
+     */
+    put: {
+        200: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/profile',
+                operation: 'put',
+                response: '200'
+            }, callback);
+        },
+        default: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/profile',
+                operation: 'put',
+                response: 'default'
+            }, callback);
+        }
+    }
+};
---
+++ api/handlers/tidbit.js
@@ -0,0 +1,77 @@
+'use strict';
+var dataProvider = require('../data/tidbit.js');
+/**
+ * Operations on /tidbit
+ */
+module.exports = {
+    /**
+     * summary: 
+     * description: Gets `Tidbit` objects.
+Optional query param of **size** determines
+size of returned array
+
+     * parameters: size
+     * produces: 
+     * responses: 200, default
+     */
+    get: function (req, res, next) {
+        /**
+         * Get the data for response 200
+         * For response `default` status 200 is used.
+         */
+        var status = 200;
+        var provider = dataProvider['get']['200'];
+        provider(req, res, function (err, data) {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.status(status).send(data && data.responses);
+        });
+    },
+    /**
+     * summary: 
+     * description: Create a new `tidbit`
+
+     * parameters: tidbit
+     * produces: 
+     * responses: 200
+     */
+    post: function (req, res, next) {
+        /**
+         * Get the data for response 200
+         * For response `default` status 200 is used.
+         */
+        var status = 200;
+        var provider = dataProvider['post']['200'];
+        provider(req, res, function (err, data) {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.status(status).send(data && data.responses);
+        });
+    },
+    /**
+     * summary: 
+     * description: 
+     * parameters: tidbit
+     * produces: 
+     * responses: 200, default
+     */
+    put: function (req, res, next) {
+        /**
+         * Get the data for response 200
+         * For response `default` status 200 is used.
+         */
+        var status = 200;
+        var provider = dataProvider['put']['200'];
+        provider(req, res, function (err, data) {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.status(status).send(data && data.responses);
+        });
+    }
+};
