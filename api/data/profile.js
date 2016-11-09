5afe2cd3b4b0ec8b68487b148f5cc541d8d41154
Travis Robison <travis.robison@progfinance.com>
2016-11-08 21:10:18 -0700

    update api

--- api/data/profile.js
+++ api/data/profile.js
@@ -6,14 +6,33 @@ var Mockgen = require('./mockgen.js');
 module.exports = {
     /**
      * summary: 
-     * description: Gets `Profile` objects.
-Optional query param of **size** determines
-size of returned array
+     * description: Create a new `profile`
 
-     * parameters: size
+     * parameters: profile
+     * produces: application/json
+     * responses: 200
+     * operationId: 
+     */
+    post: {
+        200: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/profile',
+                operation: 'post',
+                response: '200'
+            }, callback);
+        }
+    },
+    /**
+     * summary: Find profiles by ID
+     * description: Get a profile by userId
+     * parameters: 
      * produces: application/json
      * responses: 200, default
-     * operationId: 
+     * operationId: getProfileById
      */
     get: {
         200: function (req, res, callback) {
@@ -41,30 +60,8 @@ size of returned array
     },
     /**
      * summary: 
-     * description: Create a new `profile`
-
-     * parameters: profile
-     * produces: application/json
-     * responses: 200
-     * operationId: 
-     */
-    post: {
-        200: function (req, res, callback) {
-            /**
-             * Using mock data generator module.
-             * Replace this by actual data for the api.
-             */
-            Mockgen().responses({
-                path: '/profile',
-                operation: 'post',
-                response: '200'
-            }, callback);
-        }
-    },
-    /**
-     * summary: 
      * description: 
-     * parameters: profile
+     * parameters: data
      * produces: application/json
      * responses: 200, default
      * operationId: 
