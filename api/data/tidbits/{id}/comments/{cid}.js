96226e2d7516df383f7b34be514d9446af129683
Travis Robison <travis.robison@progfinance.com>
2016-11-10 22:32:18 -0700

    comments are working now!  Api is done

--- api/data/tidbits/{id}/comments/{cid}.js
+++ api/data/tidbits/{id}/comments/{cid}.js
@@ -1,5 +1,7 @@
 'use strict';
 var Mockgen = require('../../../mockgen.js');
+var Comment = require(dirname + '/app/models/swagifiedApi.js').Comment;
+var _ = require('lodash');
 /**
  * Operations on /tidbits/{id}/comments/{cid}
  */
@@ -14,15 +16,14 @@ module.exports = {
      */
     get: {
         200: function (req, res, callback) {
-            /**
-             * Using mock data generator module.
-             * Replace this by actual data for the api.
-             */
-            Mockgen().responses({
-                path: '/tidbits/{id}/comments/{cid}',
-                operation: 'get',
-                response: '200'
-            }, callback);
+
+            Comment.findOne({tidbitId: req.params.id, _id: req.params.cid},function(err, theComment){
+                if (err){
+                    return callback(err);
+                } else {
+                    return callback(null, {responses: theComment});
+                }
+            });
         },
         default: function (req, res, callback) {
             /**
@@ -46,15 +47,21 @@ module.exports = {
      */
     put: {
         200: function (req, res, callback) {
-            /**
-             * Using mock data generator module.
-             * Replace this by actual data for the api.
-             */
-            Mockgen().responses({
-                path: '/tidbits/{id}/comments/{cid}',
-                operation: 'put',
-                response: '200'
-            }, callback);
+
+           Comment.findOne({tidbitId: req.params.id, _id: req.params.cid},function(err, theComment){
+               if (err){
+                   return callback(err);
+               } else {
+                   _.merge(theComment, req.body);
+                   theComment.save(function(err, updatedComment){
+                       if(err){
+                           return err;
+                       }else {
+                            return callback(null, {responses: updatedComment});
+                       }
+                   });
+               }
+           });
         },
         default: function (req, res, callback) {
             /**
