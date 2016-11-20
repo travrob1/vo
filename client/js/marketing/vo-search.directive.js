fa489741822c655528f5566a3f16955b81712c38
Travis Robison <travis.robison@progfinance.com>
2016-11-19 21:15:52 -0700

    search on home page

---
+++ client/js/marketing/vo-search.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+/*global angular */
+
+angular.module('app').directive('voSearch', function(){
+    return {
+        restrict: 'AE',
+        templateUrl: 'js/marketing/search.html',
+        link: function(scope, elem, attrs){
+            
+        }
+    };
+}); 
