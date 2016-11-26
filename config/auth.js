76515b06bbef9efc4338aa7cd084ebf384534a80
Travis Robison <travis.robison@progfinance.com>
2016-11-25 23:21:43 -0700

    update Facebook auth

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '1003004099767925', // your App ID
-		'clientSecret' 	: '7179c53cd3b4e8df65b1f01d07f6b3ad', // your App Secret
+		'clientID' 		: '365784007109876', // your App ID
+		'clientSecret' 	: 'a488fcf5bbeefb4983659728ac1f76a7', // your App Secret
 		'callbackURL' 	: '/auth/facebook/callback',
     	'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,gender,last_name,link,locale,name,timezone,verified,email,updated_time,picture.type(large)'
 
