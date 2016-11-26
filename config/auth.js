65a3672e7e9d7a664d0a3c1da2c54d37ddcd1a9d
Travis Robison <travis.robison@progfinance.com>
2016-11-25 18:45:53 -0700

    modify auth urls

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '1003004099767925', // your App ID
 		'clientSecret' 	: '7179c53cd3b4e8df65b1f01d07f6b3ad', // your App Secret
-		'callbackURL' 	: 'http://localhost:4000/auth/facebook/callback',
+		'callbackURL' 	: '/auth/facebook/callback',
     	'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,gender,last_name,link,locale,name,timezone,verified,email,updated_time,picture.type(large)'
 
 	},
@@ -20,7 +20,7 @@ module.exports = {
 	'googleAuth' : {
 		'clientID' 		: '174222772059-sfg2577ht7hgnd63q12d6hpc1994lhim.apps.googleusercontent.com',
 		'clientSecret' 	: 'G1To4veAB-q8N5VUhDe6zFJ3',
-		'callbackURL' 	: 'http://localhost:4000/auth/google/callback'
+		'callbackURL' 	: '/auth/google/callback'
 	}
 
 };
